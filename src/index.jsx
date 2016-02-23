2bb47398ea86848c31b3ec3d3c71794f4b654442
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-02-23 22:44:50 +0300

    Setup redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import effects from 'redux-effects';
+import fetch from 'redux-effects-fetch';
 import routes from './routes';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(effects, fetch));
 
 ReactDOM.render(
-  <Router history={ browserHistory } routes={ routes } />,
+  <Provider store={store}>
+    <Router history={ browserHistory } routes={ routes } />
+  </Provider>,
   document.getElementById('app'));
