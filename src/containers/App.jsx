1b78558c09a4a2646c475f3ee651e2f549a0aeee
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-02-23 22:36:36 +0300

    Add react-router support

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-export default class App extends Component {
+const propTypes = {
+  children: PropTypes.object,
+};
+
+class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        {this.props.children}
       </div>
     );
   }
 }
+
+App.propTypes = propTypes;
+
+export default App;
